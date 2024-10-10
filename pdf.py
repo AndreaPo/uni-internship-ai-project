b62667dedcdb841fee07c2873037fdb2953e0a43
Andrea <andrea.pollio@outlook.com>
2024-10-10 12:13:47 +0200

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -5,7 +5,7 @@ class Pdf(fpdf.FPDF):
         #set the font, bold, font-size
         self.set_font('Arial', 'B', 15)
         self.cell(80)
-        self.cell(30,10,"Esercise",border=0,ln=0)
+        self.cell(30,10,"Exercise",border=0,ln=0)
         self.ln(20)
     def footer(self):
         self.set_y(-15)
